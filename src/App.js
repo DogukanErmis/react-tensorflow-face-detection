789dfec433bc660d860d887383601ec32fbbf158
dogukanermis <ermisd+github@pm.me>
2022-01-27 17:09:45 +0100

    display default image containing face

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img
+        src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt="default image"
+        width="500"
+      />
+      <canvas width="500" />
+    </div>
+  );
 }
 
 export default App;
