2e3f2378116796b42b0a7bb7997371c2c264004c
dogukanermis <ermisd+github@pm.me>
2022-01-27 17:15:04 +0100

    create references for img and canvas tags

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  const imgRef = useRef();
+  const canvasRef = useRef();
+
   return (
     <div className="App">
       <img
+        ref={imgRef}
         src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt="default image"
+        alt="default"
         width="500"
+        height="750"
       />
-      <canvas width="500" />
+      <canvas ref={canvasRef} width="500" height="750" />
     </div>
   );
 }
