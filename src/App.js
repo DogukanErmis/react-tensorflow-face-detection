0d00ce197fad90930b8d6762087bd3423a6daafa
dogukanermis <ermisd+github@pm.me>
2022-01-27 17:55:34 +0100

    display face detection canvas on image

--- src/App.js
+++ src/App.js
@@ -6,18 +6,43 @@ function App() {
   const imgRef = useRef();
   const canvasRef = useRef();
 
+  const handleDetection = async () => {
+    const detections = await faceapi.detectAllFaces(
+      imgRef.current,
+      new faceapi.TinyFaceDetectorOptions()
+    );
+
+    canvasRef.current.innerText = faceapi.createCanvasFromMedia(imgRef.current);
+
+    faceapi.matchDimensions(canvasRef.current, {
+      width: 500,
+      height: 750,
+    });
+
+    const resized = faceapi.resizeResults(detections, {
+      width: 500,
+      height: 750,
+    });
+
+    faceapi.draw.drawDetections(canvasRef.current, resized);
+  };
+
   useEffect(() => {
     const loadModels = () => {
       faceapi.nets.tinyFaceDetector
         .loadFromUri("/models")
-        .then(console.log("done"))
+        .then(handleDetection)
         .catch((e) => console.log(e));
     };
+
+    // Run loadModels() after img has loaded
+    imgRef.current && loadModels();
   }, []);
 
   return (
     <div className="App">
       <img
+        crossOrigin="anonymous"
         ref={imgRef}
         src="https://images.pexels.com/photos/9371782/pexels-photo-9371782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
         alt="default"
